45b86e4eff4e112f51fd267c8221984665db14aa
eliguus <naoltd164@gmail.com>
2024-07-08 23:19:06 +0300

    vercel reconfigured

--- routes/public.js
+++ routes/public.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const indexRouter = express.Router()
-const userController = require("../controllers/userController")
+const userController = require("../controllers/usersController")
 indexRouter.post("/auth/register", userController.registerUser)
 indexRouter.post("/auth/login", userController.loginUser)
 module.exports = indexRouter
